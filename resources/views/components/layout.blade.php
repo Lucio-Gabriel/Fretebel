edefdae15d4afcd9417b559013cbc82e971eb554
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-06-24 11:09:54 -0300

    feat: create of component of layout and register routes

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $title ?? 'Meu App' }}</title>
+    <title>{{ $title ?? 'Fretebel' }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-gray-100 min-h-screen">
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login-customer', function () {
+    return view('Auth.customers.login');
+})->name('login-customer');
+
+Route::get('/login-driver', function () {
+    return view('Auth.drivers.login');
+})->name('login-driver');
